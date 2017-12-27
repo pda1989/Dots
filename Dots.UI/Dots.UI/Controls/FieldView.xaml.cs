430091cd953aca6a20d48616f914246524767806
Dmitry Pokhlebaev <pokhlebaev.dmitry@gmail.com>
2017-12-28 00:03:07 +0100

    add field control

---
+++ Dots.UI/Dots.UI/Controls/FieldView.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Dots.Core.Field;
+using Dots.Core.Game;
+using Dots.UI.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Dots.UI.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FieldView : ContentView, IGameFieldPainter
+    {
+        #region Fields
+
+        private static readonly BindableProperty TappedCommandProperty =
+            BindableProperty.Create("TappedCommand", typeof(ICommand), typeof(DotView));
+
+        private List<List<DotView>> _controls;
+        private int _fieldSize;
+        private Grid _grid;
+
+        #endregion
+
+        #region Constructors
+
+        public FieldView()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ICommand TappedCommand
+        {
+            get => (ICommand) GetValue(TappedCommandProperty);
+            set => SetValue(TappedCommandProperty, value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Paint(Field field)
+        {
+            if (_grid == null)
+            {
+                _grid = new Grid
+                {
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                    BackgroundColor = Color.DarkGray,
+                    Padding = new Thickness(10, 10, 10, 10),
+                    Margin = new Thickness(10, 10, 10, 10),
+                    RowSpacing = 5,
+                    ColumnSpacing = 5
+                };
+
+                _grid.SizeChanged += (s, e) =>
+                {
+                    if (Height <= Width)
+                    {
+                        _grid.WidthRequest = _grid.Height;
+                        _grid.HeightRequest = _grid.Height;
+                    }
+                    else
+                    {
+                        _grid.HeightRequest = _grid.Width;
+                        _grid.WidthRequest = _grid.Width;
+                    }
+                };
+
+                Content = _grid;
+            }
+
+            if (_fieldSize != field.Size)
+            {
+                _controls = new List<List<DotView>>();
+
+                _grid.RowDefinitions.Clear();
+                _grid.ColumnDefinitions.Clear();
+
+                for (var i = 0; i < field.Size; i++)
+                {
+                    _grid.RowDefinitions.Add(new RowDefinition());
+                    _grid.ColumnDefinitions.Add(new ColumnDefinition());
+                }
+
+                for (var i = 0; i < field.Size; i++)
+                {
+                    _controls.Add(new List<DotView>());
+                    for (var j = 0; j < field.Size; j++)
+                    {
+                        var dotView = new DotView
+                        {
+                            Source = new DotModel
+                            {
+                                Row = i,
+                                Column = j,
+                                Dot = field[i][j]
+                            },
+                            TappedCommand = TappedCommand
+                        };
+
+                        _grid.Children.Add(dotView, j, i);
+                        _controls[i].Add(dotView);
+                    }
+                }
+
+                _fieldSize = field.Size;
+            }
+
+            for (var i = 0; i < field.Size; i++)
+            for (var j = 0; j < field.Size; j++)
+                if (_controls[i][j].Source?.Dot != field[i][j])
+                    _controls[i][j].Source = new DotModel
+                    {
+                        Row = i,
+                        Column = j,
+                        Dot = field[i][j]
+                    };
+        }
+
+        #endregion
+    }
+}
--- Dots.UI/Dots.UI/MainPage.xaml.cs
+++ Dots.UI/Dots.UI/MainPage.xaml.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using Dots.Core.Field;
-using Dots.Core.Game;
+﻿using Dots.Core.Game;
 using Dots.UI.Controls;
 using Dots.UI.Models;
 using Xamarin.Forms;
 
 namespace Dots.UI
 {
-    public partial class MainPage : ContentPage, IGameFieldPainter
+    public partial class MainPage : ContentPage
     {
         #region Fields
 
         private readonly Game _game;
-        private List<List<DotView>> _controls;
-        private int _fieldSize;
-        private Grid _grid;
 
         #endregion
 
@@ -25,112 +19,36 @@ namespace Dots.UI
         {
             InitializeComponent();
 
-            _game = new Game(this);
-
-            _game.Initialyze(10);
-
-            _game.Paint();
-        }
-
-        #endregion
-
-        #region Methods
-
-        public void Paint(Field field)
-        {
-            if (_grid == null)
+            var field = new FieldView
             {
-                _grid = new Grid
+                TappedCommand = new Command(dotModel =>
                 {
-                    HorizontalOptions = LayoutOptions.Center,
-                    VerticalOptions = LayoutOptions.Center,
-                    BackgroundColor = Color.DarkGray,
-                    Padding = new Thickness(10, 10, 10, 10),
-                    Margin = new Thickness(10, 10, 10, 10),
-                    RowSpacing = 5,
-                    ColumnSpacing = 5
-                };
-
-                _grid.SizeChanged += (s, e) =>
-                {
-                    if (Height <= Width)
-                    {
-                        _grid.WidthRequest = _grid.Height;
-                        _grid.HeightRequest = _grid.Height;
-                    }
-                    else
-                    {
-                        _grid.HeightRequest = _grid.Width;
-                        _grid.WidthRequest = _grid.Width;
-                    }
-                };
-            }
-
-            if (_fieldSize != field.Size)
-            {
-                _controls = new List<List<DotView>>();
-
-                _grid.RowDefinitions.Clear();
-                _grid.ColumnDefinitions.Clear();
-
-                for (var i = 0; i < field.Size; i++)
-                {
-                    _grid.RowDefinitions.Add(new RowDefinition());
-                    _grid.ColumnDefinitions.Add(new ColumnDefinition());
-                }
-
-                for (var i = 0; i < field.Size; i++)
-                {
-                    _controls.Add(new List<DotView>());
-                    for (var j = 0; j < field.Size; j++)
-                    {
-                        var dotView = new DotView
+                    if (dotModel is DotModel model)
+                        try
                         {
-                            Source = new DotModel
+                            _game?.MakeMove(model.Row, model.Column);
+                            _game?.Paint();
+
+                            if (_game != null)
                             {
-                                Row = i,
-                                Column = j,
-                                Dot = field[i][j]
-                            },
-                            TappedCommand = new Command(dotModel =>
-                            {
-                                if (dotModel is DotModel model)
-                                {
-                                    try
-                                    {
-                                        _game.MakeMove(model.Row, model.Column);
-                                        _game.Paint();
-                                    }
-                                    catch
-                                    {
-                                        // ignored
-                                    }
-                                }
-                            })
-                        };
+                                MoveLabel.Text = _game.FirstPlayerMove ? "First player" : "Second player";
+                                MoveLabel.TextColor = _game.FirstPlayerMove ? Color.Blue : Color.Brown;
+                                ScoresLabel.Text =
+                                    $"Score: {_game.Result.FirstPlayerScore} : {_game.Result.SecondPlayerScore}";
+                            }
+                        }
+                        catch
+                        {
+                            // ignored
+                        }
+                })
+            };
 
-                        _grid.Children.Add(dotView, j, i);
-                        _controls[i].Add(dotView);
-                    }
-                }
+            ParentGrid.Children.Add(field, 0, 1);
 
-                _fieldSize = field.Size;
-            }
-
-            for (var i = 0; i < field.Size; i++)
-            for (var j = 0; j < field.Size; j++)
-                if (_controls[i][j].Source?.Dot != field[i][j])
-                    _controls[i][j].Source = new DotModel
-                    {
-                        Row = i,
-                        Column = j,
-                        Dot = field[i][j]
-                    };
-
-            MoveLabel.Text = _game.FirstPlayerMove ? "First player" : "Second player";
-            MoveLabel.TextColor = _game.FirstPlayerMove ? Color.Blue : Color.Brown;
-            ScoresLabel.Text = $"Score: {_game.Result.FirstPlayerScore} : {_game.Result.SecondPlayerScore}";
-            ParentGrid.Children.Add(_grid, 0, 1);
+            _game = new Game(field);
+            _game.Initialyze(10);
+            _game.Paint();
         }
 
         #endregion
